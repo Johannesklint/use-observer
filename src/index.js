22c08a8a576a1e3344249cd7abf9205fccd326b7
johkli <johannes.klint@etraveligroup.com>
2020-03-19 16:20:07 +0100

    Inital commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { useRef, useCallback } from 'react';
+
+const useObserver = (callback, ref) => {
+  const internalRef = useRef();
+  const observer = new IntersectionObserver(callback, {
+    root: null,
+    threshold: 1.0,
+  });
+
+  const setRef = useCallback(
+    node => {
+      if (internalRef.current) {
+        observer.unobserve(internalRef.current);
+      }
+      if (node) {
+        observer.observe(node);
+      }
+
+      internalRef.current = node;
+    },
+    [observer, ref],
+  );
+
+  return setRef;
+};
+
+export default useObserver;
