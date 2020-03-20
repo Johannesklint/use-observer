7cac7f11866dbc0a01f86140956835fbe006e114
johkli <johannes.klint@etraveligroup.com>
2020-03-20 16:29:29 +0100

    Add tests

---
+++ src/__tests__/useObserver.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import useObserver from '../useObserver';
+
+describe('useObserver', () => {
+  const observeMock = {
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+  };
+
+  beforeAll(() => {
+    window.IntersectionObserver = jest.fn(() => observeMock);
+  });
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  const callback = jest.fn();
+  let dummyRef;
+  const Dummy = () => {
+    const ref = useObserver(callback);
+    return (
+      <div
+        ref={node => {
+          ref(node);
+          dummyRef = node;
+        }}
+      />
+    );
+  };
+
+  test('observer calls callback and internal functions', () => {
+    const { unmount } = render(<Dummy />);
+
+    expect(observeMock.observe).toHaveBeenCalledWith(dummyRef);
+    expect(window.IntersectionObserver).toHaveBeenCalledWith(callback, {
+      root: null,
+      threshold: 1.0,
+    });
+
+    unmount();
+    expect(observeMock.unobserve).toHaveBeenCalled();
+  });
+});
